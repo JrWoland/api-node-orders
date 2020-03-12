15de3f11b88fd627e9c18a2e49acaa9f79cca6da
Marcin Kozicki <m.kozicki2@gmail.com>
2020-03-13 00:28:16 +0100

    Add more descriptive api responses

--- api/routes/products.js
+++ api/routes/products.js
@@ -8,10 +8,20 @@ const Product = require('../models/products');
 
 router.get('/', (req, res, next) => {
   Product.find()
+    .select('name price _id')
     .exec()
     .then(docs => {
-      console.log(docs);
-      res.status(200).json(docs);
+      console.log(req);
+      const response = {
+        count: docs.length,
+        products: docs.map(doc => ({
+          name: doc.name,
+          price: doc.price,
+          id: doc._id,
+          request: { type: req.method }
+        }))
+      };
+      res.status(200).json(response);
     })
     .catch(err => {
       console.log(err);
@@ -31,8 +41,12 @@ router.post('/', (req, res, next) => {
     .then(result => {
       console.log(result);
       res.status(201).json({
-        message: 'Handling post /products',
-        createdProduct: product
+        message: 'Product created ',
+        createdProduct: {
+          name: result.name,
+          price: result.price,
+          id: result._id
+        }
       });
     })
     .catch(err => {
