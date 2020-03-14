71fc1abb5dae3250e687c7dc9beccb1c5577eefc
Marcin Kozicki <m.kozicki2@gmail.com>
2020-03-14 19:08:42 +0100

    Create POST and GET methods to manage orders

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -2,21 +2,53 @@ const express = require('express');
 
 const router = express.Router();
 
+const mongsoose = require('mongoose');
+
+const Order = require('../models/orders');
+
 router.get('/', (req, res, next) => {
-  res.status(200).json({
-    message: 'Handling GET /orders'
-  });
+  Order.find()
+    .select('product quantity _id')
+    .exec()
+    .then(docs => {
+      const response = {
+        count: docs.length,
+        order: docs.map(doc => ({
+          id: doc._id,
+          product: doc.product,
+          quantity: doc.quantity
+        }))
+      };
+      res.status(200).json(response);
+    })
+    .catch(err => res.status(500).json({ error: err }));
 });
 
 router.post('/', (req, res, next) => {
-  const order = {
-    productId: req.body.productId,
-    quantity: req.body.quantity
-  };
-  res.status(201).json({
-    message: 'Handling post /orders',
-    order: order
+  const order = new Order({
+    _id: mongsoose.Types.ObjectId(),
+    quantity: req.body.quantity,
+    product: req.body.productId
   });
+  order
+    .save()
+    .then(result => {
+      console.log(result);
+      res.status(201).json({
+        message: 'Order stored',
+        createdOrder: {
+          id: result._id,
+          product: result.product,
+          quantity: result.quantity
+        }
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      });
+    });
 });
 
 router.get('/:orderId', (req, res, next) => {
